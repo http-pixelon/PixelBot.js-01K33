bae67ed391f63e11651deefa630bb431833e1494
Kauã Frigieri <41579326+httppixelon@users.noreply.github.com>
2023-02-25 04:55:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// ------------------------------------
+// INDEX ANTIGA DO BOT | Discord.JS V12
+// ------------------------------------
+
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
