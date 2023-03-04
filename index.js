fed45615f700e913c87ebf01b0dcb8596b97cd34
Kauã Frigieri <41579326+http-pixelon@users.noreply.github.com>
2023-03-04 00:00:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ const config = require("./config.json"); //Pegando o prefixo do bot para respost
 
 client.login(process.env.TOKEN); //Ligando o Bot caso ele consiga acessar o token
 
-30000, 30000,
+30000, 30000, 30000,
 
